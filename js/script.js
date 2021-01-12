38d7d8fb282f13d2ccfb2d07c4a4a877f4a5ec89
Ezekiel <Eazyberry58@gmail.com>
2021-01-12 03:47:45 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,474 @@
+setTimeout(function() {
+    $('.loader_bg').fadeToggle();
+}, 1500);
+//nav scripting
+$(window).scroll(function() {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+});
+
+$(window).scroll(function() {
+    $('.nav-link').toggleClass('scrolled', $(this).scrollTop() > 100);
+});
+//End of nav scripting
+
+
+//Read more scripting
+var i = 0;
+
+function chairman_read() {
+    if (!i) {
+        document.getElementById("more").style.display = "inline";
+
+        document.getElementById("read").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more").style.display = "none";
+
+        document.getElementById("read").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+var i = 0;
+
+function read_2() {
+    if (!i) {
+        document.getElementById("more_2").style.display = "inline";
+
+        document.getElementById("read_2").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more_2").style.display = "none";
+
+        document.getElementById("read_2").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+
+
+
+var i = 0;
+
+function read_3() {
+    if (!i) {
+        document.getElementById("more").style.display = "inline";
+
+        document.getElementById("read_3").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more").style.display = "none";
+
+        document.getElementById("read_3").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_4() {
+    if (!i) {
+        document.getElementById("more4").style.display = "inline";
+
+        document.getElementById("read_4").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more4").style.display = "none";
+
+        document.getElementById("read_4").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_5() {
+    if (!i) {
+        document.getElementById("more5").style.display = "inline";
+
+        document.getElementById("read_5").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more5").style.display = "none";
+
+        document.getElementById("read_5").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+function read_6() {
+    if (!i) {
+        document.getElementById("more6").style.display = "inline";
+
+        document.getElementById("read_6").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more6").style.display = "none";
+
+        document.getElementById("read_6").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+function read_7() {
+    if (!i) {
+        document.getElementById("more7").style.display = "inline";
+
+        document.getElementById("read_7").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more7").style.display = "none";
+
+        document.getElementById("read_7").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+function read_8() {
+    if (!i) {
+        document.getElementById("more8").style.display = "inline";
+
+        document.getElementById("read_8").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more8").style.display = "none";
+
+        document.getElementById("read_8").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+function read_9() {
+    if (!i) {
+        document.getElementById("more9").style.display = "inline";
+
+        document.getElementById("read_9").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more9").style.display = "none";
+
+        document.getElementById("read_9").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_10() {
+    if (!i) {
+        document.getElementById("more10").style.display = "inline";
+
+        document.getElementById("read_10").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more10").style.display = "none";
+
+        document.getElementById("read_10").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+function read_11() {
+    if (!i) {
+        document.getElementById("more11").style.display = "inline";
+
+        document.getElementById("read_11").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more11").style.display = "none";
+
+        document.getElementById("read_11").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_12() {
+    if (!i) {
+        document.getElementById("more12").style.display = "inline";
+
+        document.getElementById("read_12").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more12").style.display = "none";
+
+        document.getElementById("read_12").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_13() {
+    if (!i) {
+        document.getElementById("more13").style.display = "inline";
+
+        document.getElementById("read_13").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more13").style.display = "none";
+
+        document.getElementById("read_13").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_14() {
+    if (!i) {
+        document.getElementById("more14").style.display = "inline";
+
+        document.getElementById("read_14").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more14").style.display = "none";
+
+        document.getElementById("read_14").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_15() {
+    if (!i) {
+        document.getElementById("more15").style.display = "inline";
+
+        document.getElementById("read_15").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more15").style.display = "none";
+
+        document.getElementById("read_15").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_16() {
+    if (!i) {
+        document.getElementById("more16").style.display = "inline";
+
+        document.getElementById("read_16").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more16").style.display = "none";
+
+        document.getElementById("read_16").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_17() {
+    if (!i) {
+        document.getElementById("more17").style.display = "inline";
+
+        document.getElementById("read_17").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more17").style.display = "none";
+
+        document.getElementById("read_17").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_18() {
+    if (!i) {
+        document.getElementById("more18").style.display = "inline";
+
+        document.getElementById("read_18").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more18").style.display = "none";
+
+        document.getElementById("read_18").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_19() {
+    if (!i) {
+        document.getElementById("more19").style.display = "inline";
+
+        document.getElementById("read_19").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more19").style.display = "none";
+
+        document.getElementById("read_19").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_20() {
+    if (!i) {
+        document.getElementById("more20").style.display = "inline";
+
+        document.getElementById("read_20").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more20").style.display = "none";
+
+        document.getElementById("read_20").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_21() {
+    if (!i) {
+        document.getElementById("more21").style.display = "inline";
+
+        document.getElementById("read_21").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more21").style.display = "none";
+
+        document.getElementById("read_21").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_22() {
+    if (!i) {
+        document.getElementById("more22").style.display = "inline";
+
+        document.getElementById("read_22").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more22").style.display = "none";
+
+        document.getElementById("read_22").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_23() {
+    if (!i) {
+        document.getElementById("more23").style.display = "inline";
+
+        document.getElementById("read_23").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more23").style.display = "none";
+
+        document.getElementById("read_23").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+function read_24() {
+    if (!i) {
+        document.getElementById("more24").style.display = "inline";
+
+        document.getElementById("read_24").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more24").style.display = "none";
+
+        document.getElementById("read_24").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+var i = 0;
+
+function read_1() {
+    if (!i) {
+        document.getElementById("more_1").style.display = "inline";
+
+        document.getElementById("read_1").innerHTML = "Read less";
+
+        i = 1;
+    } else {
+        document.getElementById("more_1").style.display = "none";
+
+        document.getElementById("read_1").innerHTML = "Read more";
+
+        i = 0;
+
+    }
+}
+
+// $(document).ready(function(){
+//     $(".reads").click(function(){
+//         $(this).prev().toggle();
+//     });
+// });
+//End of Read more scripting
+
+baguetteBox.run('.cards-gallery', { animation: 'slideIn' });
+//contact page scripting
+var x = document.getElementById("contact_1");
+
+var y = document.getElementById("contact_2");
+
+var z = document.getElementById("btn");
+
+function contact_2() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px"
+}
+
+
+function contact_1() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px"
+}
+//End of contact page scripting
